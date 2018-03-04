50449402e68bb22858909017b91337d9887bb56a
Joao Miguel Neves <joaoneves792@gmail.com>
2018-03-04 16:09:03 +0000

    Better result processing

--- tests/results/topology5_1host/plotter_server.py
+++ tests/results/topology5_1host/plotter_server.py
@@ -1,6 +1,7 @@
 import numpy
 import Gnuplot
 import re
+from glob import glob
 
 timestamp_re = re.compile(r"^\d+$")
 throughput_re = re.compile(r"\[.*\]\s*(\d+)\.\d+-\d+\.\d+\s+sec\s+[0-9\.]+\s+\w+\s+([0-9\.]+)\s+(\w+)/sec\s+(\d+)\s+[0-9\.]+\s+\w+")
@@ -9,7 +10,9 @@ throughput_server_udp_re = re.compile(r"^\[.*\]\s*(\d+)\.\d+-\d+\.\d+\s+sec\s+[0
 Kbps_re = re.compile(r"^Kbits")
 Mbps_re = re.compile(r"^Mbits")
 bps_re = re.compile(r"^bits")
-
+server_re = re.compile(r"^Server listening on 5201\s*$")
+client_connection_re = re.compile(r"^\[\s+\d+\] local (\d+\.\d+\.\d+\.\d+).+5201\s*$")
+server_connection_re = re.compile(r"^Accepted connection from (\d+\.\d+\.\d+\.\d+), port \d+\s*$")
 
 def parse_iperf(client, server, udp):
     start_timestamp = 0
@@ -41,6 +44,28 @@ def parse_iperf(client, server, udp):
 
     return Kbps
 
+def tag_file(path):
+    file = open(path)
+    lines = file.readlines()
+    server = False
+    client = False
+    ip = ""
+    for line in lines:
+        if server_re.match(line):
+            server = True
+        if timestamp_re.match(line):
+            client = True
+        if server_connection_re.match(line):
+            ip = server_connection_re.findall(line)[0]
+        if client_connection_re.match(line):
+            ip = client_connection_re.findall(line)[0]
+    file.close()
+
+    if (not (client or server)) or not ip:
+        return None
+
+    return (server, ip, path)
+
 def overlap(*d):
     lower_bound = 0
     upper_bound = d[0][-1][0]
@@ -60,72 +85,101 @@ def overlap(*d):
             else:
                 break
 
-def trim(n, *d):
+def trim(start, end, *d):
     for dataset in d:
-        del dataset[:n]
-        del dataset[-n:]
+        del dataset[:start]
+        del dataset[-end:]
+
+def pad(*d):
+    lower_bound = d[0][-1][0]
+    upper_bound = 0
+    for dataset in d:
+        if dataset[0][0] < lower_bound:
+            lower_bound = dataset[0][0]
+        if dataset[-1][0] > upper_bound:
+            upper_bound = dataset[-1][0]
+
+
+    for dataset in d:
+        while True:
+            if dataset[0][0] > lower_bound:
+                dataset.insert(0,(dataset[0][0]-1, 0))
+            elif dataset[-1][0] < upper_bound:
+                dataset.append((dataset[-1][0]+1, 0))
+            else:
+                break
 
 
 def main():
 
-    folder = "run8/"
+    ### CONFIGURATION #####
+    #folder = "run6_server/"
+    #folder = "run7_EWMA/"
+    #folder = "run8/"
+    folder = "run9_sync_buffers/"
+    interesting_files = "*.log"
     udp = False
+    #######################
 
-    client1_file = folder+"c1.log"
-    client2_file = folder+"c2.log"
-    client3_file = folder+"c3.log"
+    files = glob(folder+interesting_files)
+    data_files = []
+    for name in files:
+        tag = tag_file(name)
+        if tag is not None:
+            data_files.append(tag)
+    pairing = []
+    for tag1 in data_files:
+        for tag2 in data_files:
+            if tag1[0] and not tag2[0]:  # tag1 is a server and tag2 is a client
+                if tag1[1] == tag2[1]:  # ips match
+                    pairing.append((tag1, tag2))
 
-    server1_file = folder+"s1.log"
-    server2_file = folder+"s2.log"
-    server3_file = folder+"s3.log"
+    data_sets = []
+    for pairs in pairing:
+        fs = open(pairs[0][2])
+        fc = open(pairs[1][2])
+        d = parse_iperf(fc, fs, udp)
+        fs.close()
+        fc.close()
+        data_sets.append(d)
 
-    fc = open(client1_file)
-    fs = open(server1_file)
-    d1 = parse_iperf(fc, fs, udp)
-    fc.close()
-    fs.close()
+    print("Got " + str(len(data_sets)) + " data sets")
 
-    fc = open(client2_file)
-    fs = open(server2_file)
-    d2 = parse_iperf(fc, fs, udp)
-    fc.close()
-    fs.close()
+    #trim(0, 650, *data_sets)
+    #pad(*data_sets)
 
-    fc = open(client3_file)
-    fs = open(server3_file)
-    d3 = parse_iperf(fc, fs, udp)
-    fc.close()
-    fs.close()
-
-    overlap(d1, d2, d3)
-    trim(60, d1, d2, d3)
-
-    t = numpy.array([x[0] for x in d1])
-    c1 = numpy.array([x[1] for x in d1])
-    c2 = numpy.array([x[1] for x in d2])
-    c3 = numpy.array([x[1] for x in d3])
-    s = c1+c2+c3
-
-    data = [("Client1", c1, 17777),
-            ("Client2", c2, 22222),
-            ("Client3", c3, 10000),
-            ("Total", s, 50000)]
+    overlap(*data_sets)
+    trim(60, 60, *data_sets)
 
     g = Gnuplot.Gnuplot()
-    gd1 = Gnuplot.Data(c1, title="client1", with_="lines")
-    gd2 = Gnuplot.Data(c2, title="client2", with_="lines")
-    gd3 = Gnuplot.Data(c3, title="client3", with_="lines")
-    gdsum = Gnuplot.Data(s, title="Total", with_="lines")
 
-    g.plot(gd1, gd2, gd3, gdsum)
+    numpy_arrays = []
+    gnuplot_data = []
+    total = None
+    for i, data in enumerate(data_sets):
+        name = "Client"+str(i+1)
+        c = numpy.array([x[1] for x in data])
+        if total is None:
+            total = c.copy()
+        else:
+            total += c
+        numpy_arrays.append(c)
+        gd = Gnuplot.Data(c, title=name, with_="lines")
+        gnuplot_data.append(gd)
 
-    for d in data:
-        print(d[0])
-        print(" expected: " + str(d[2]))
-        print(" mean:     " + str(d[1].mean()))
-        print(" max:      " + str(d[1].max()))
-        print(" min:      " + str(d[1].min()))
-        print(" dev:      " + str(d[1].std()))
+    numpy_arrays.append(total)
+    gdsum = Gnuplot.Data(total, title="Total", with_="lines")
+    gnuplot_data.append(gdsum)
+
+
+    g.plot(*gnuplot_data)
+
+    for i, gd in enumerate(gnuplot_data):
+        print(gd.get_option("title"))
+        print(" mean:     " + str(numpy_arrays[i].mean()))
+        print(" max:      " + str(numpy_arrays[i].max()))
+        print(" min:      " + str(numpy_arrays[i].min()))
+        print(" dev:      " + str(numpy_arrays[i].std()))
 
 if __name__ == '__main__':
     main()
