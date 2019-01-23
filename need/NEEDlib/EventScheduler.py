b778fac5f32dcff34d96fe69c48b6cbca9c47ab6
Joao Miguel Neves <joaoneves792@gmail.com>
2019-01-23 11:15:42 +0000

    Proper precompute of link changes

--- need/NEEDlib/EventScheduler.py
+++ need/NEEDlib/EventScheduler.py
@@ -13,6 +13,7 @@ if sys.version_info >= (3, 0):
 class EventScheduler:
     def __init__(self):
         self.events = []  # type: List[Timer]
+        self.link_changes = []
 
     def start(self):
         for e in self.events:
@@ -35,52 +36,71 @@ class EventScheduler:
 
     def schedule_link_change(self, time, graph, origin, destination, bandwidth, latency, jitter, drop):
         links = []
+        new_links = []
         paths = []
         new_paths = []
+
+        # Update link info
         for l in graph.links:
             if l.source.name == origin and l.destination.name == destination:
                 links.append(l)
+                copy_l = None
 
+                # Check for previous changes
+                for changed_event in self.link_changes:
+                    changed_link = changed_event[1]  # type: NetGraph.Link
+                    if changed_link.index == l.index:
+                        copy_l = copy(changed_link)
+                        break
+
+                # if no previous changes
+                if copy_l is None:
+                    copy_l = copy(l)
+
+                # Update only what has changed
+                copy_l.bandwidth_bps = bandwidth if bandwidth >= 0 else copy_l.bandwidth_bps
+                copy_l.latency = int(latency) if latency >= 0 else copy_l.latency
+                copy_l.jitter = float(jitter) if jitter >= 0 else copy_l.jitter
+                copy_l.drop = float(drop) if drop >= 0 else copy_l.drop
+
+                new_links.append(copy_l)
+                self.link_changes.append((time, copy_l))
+                self.link_changes.sort(reverse=True, key=lambda change: change[0])  # make sure they are sorted in time
+
+        # Update path info
         for key in graph.paths:
             if not isinstance(key, NetGraph.Service): # If we get a path to a bridge, ignore
                 continue
             path = graph.paths[key]
-            for link in links:
+            for new_link in new_links:
                 for i, l in enumerate(path.links):
-                    if l.index == link.index:
+                    if l.index == new_link.index:
                         paths.append(path)  # There wont be duplicated paths since there cant be multiple changed links
                         new_path = copy(path) # in the same path (replicated links must be on different paths)
 
                         # Pre-compute the path properties
                         new_path.links = copy(path.links)
-                        new_path.links[i] = copy(l)
-
-                        new_l = new_path.links[i]
-                        new_l.bandwidth_bps = bandwidth if bandwidth >= 0 else new_l.bandwidth_bps
-                        new_l.latency = int(latency) if latency >= 0 else new_l.latency
-                        new_l.jitter = float(jitter) if jitter >= 0 else new_l.jitter
-                        new_l.drop = float(drop) if drop >= 0 else new_l.drop
+                        new_path.links[i] = new_link
                         new_path.calculate_end_to_end_properties()
                         new_paths.append(new_path)
                         break
 
-
         for link in links:
-            message("Link " + link.source.name + ":" + link.destination.name + " scheduled to change at " + str(time) + " bw" + str(bandwidth))
+            message("Link " + link.source.name + ":" + link.destination.name + " scheduled to change at " + str(time))
         for path in paths:
             message("Path to" + path.links[-1].destination.name + " scheduled to change at " + str(time))
 
         self.events.append(Timer(time, link_change,
-                                 [links, paths, new_paths, bandwidth, latency, jitter, drop]))
+                                 [links, new_links, paths, new_paths]))
 
 
-def link_change(links, paths, new_paths, bandwidth_bps, latency, jitter, drop):
-    for l in links:
+def link_change(links, new_links, paths, new_paths):
+    for i, l in enumerate(links):
         with l.lock:
-            l.bandwidth_bps = bandwidth_bps if bandwidth_bps >= 0 else l.bandwidth_bps
-            l.latency = int(latency) if latency >= 0 else l.latency
-            l.jitter = float(jitter) if jitter >= 0 else l.jitter
-            l.drop = float(drop) if drop >= 0 else l.drop
+            l.bandwidth_bps = new_links[i].bandwidth_bps
+            l.latency = new_links[i].latency
+            l.jitter = new_links[i].jitter
+            l.drop = new_links[i].drop
 
     for i, path in enumerate(paths):
         with path.lock:
@@ -94,7 +114,7 @@ def link_change(links, paths, new_paths, bandwidth_bps, latency, jitter, drop):
             service = path.links[-1].destination
             change_loss(service, path.drop)
             change_latency(service, path.latency, path.jitter)
-            # We dont explicitely change bandwidth, the emulation manager will handle that for us
+            # We dont explicitly change bandwidth, the emulation manager will handle that for us
 
             '''
             message("Path " + path.links[0].source.name + " to " + path.links[-1].destination.name +
