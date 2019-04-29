ce0f7b06184cb973ed7256701a5c05d9831530be
Paulo Gouveia <paulojlgouveia@gmail.com>
2019-04-29 19:16:12 +0100

    forgot to add deploymentGenerator.py

--- need/deploymentGenerator.py
+++ need/deploymentGenerator.py
@@ -1,11 +1,12 @@
 #! /usr/bin/python
+
 import sys
 import os
 
 from need.NEEDlib.NetGraph import NetGraph
 from need.NEEDlib.XMLGraphParser import XMLGraphParser
-from need.NEEDlib.DockerComposeFileGenerator import DockerComposeFileGenerator
-from need.NEEDlib.KubernetesManifestGenerator import KubernetesManifestGenerator
+from need.NEEDlib.deploymentGenerators.DockerComposeFileGenerator import DockerComposeFileGenerator
+from need.NEEDlib.deploymentGenerators.KubernetesManifestGenerator import KubernetesManifestGenerator
 from need.NEEDlib.utils import SHORT_LIMIT
 from need.NEEDlib.utils import print_message, print_error, print_and_fail
 
@@ -16,8 +17,23 @@ def main():
              + "    <orchestrator> can be -s for Docker Swarm or -k for Kubernetes"
         
         print_and_fail(msg)
+        
+    
+    shm_size = 8000000000
+    aeron_lib_path = "/home/daedalus/Documents/aeron4need/cppbuild/Release/lib/libaeronlib.so"
+    aeron_term_buffer_length = 2*64*1024*1024           # must be multiple of 64*1024
+    aeron_ipc_term_buffer_length = 2*64*1024*1024   	# must be multiple of 64*1024
 
+    threading_mode = 'SHARED'             # aeron uses 1 thread
+    # threading_mode = 'SHARED_NETWORK'   # aeron uses 2 threads
+    # threading_mode = 'DEDICATED'        # aeron uses 3 threads
+
+    pool_period = 0.05
+    max_flow_age = 2
+    
+    
     topology_file = sys.argv[1]
+    # TODO use argparse to support other orchestrators
     orchestrator = "kubernetes" if sys.argv[2] == "-k" else "docker"
     graph = NetGraph()
 
@@ -39,11 +55,23 @@ def main():
             msg = "Path from " + path.links[0].source.name + " to " \
                   + path.links[-1].destination.name + " is too long (over 249 hops)"
             print_and_fail(msg)
-
-    generator = KubernetesManifestGenerator(os.getcwd()+"/"+topology_file, graph) if orchestrator == "kubernetes" else DockerComposeFileGenerator(topology_file, graph)
-    generator.generate()
     
-    print("Experiment UUID: " + generator.experiment_UUID, file=sys.stderr)
+    generator = None
+    if orchestrator == "kubernetes":
+        generator = KubernetesManifestGenerator(os.getcwd() + "/" + topology_file, graph)
+
+    elif orchestrator == 'swarm':
+        generator = DockerComposeFileGenerator(topology_file, graph)
+        
+    # insert here any other generators required by new orchestrators
+    else:
+        pass
+    
+    if generator is not None:
+        generator.generate(pool_period, max_flow_age, threading_mode, shm_size, aeron_lib_path, aeron_term_buffer_length, aeron_ipc_term_buffer_length)
+        print("Experiment UUID: " + generator.experiment_UUID, file=sys.stdout)
+    else:
+        print("Failed to find a suitable generator.", file=sys.stderr)
 
 
 if __name__ == '__main__':
